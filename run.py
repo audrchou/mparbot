184afcee620fd76828560608a1bdd7c4cfdaf595
Audrey Chou <audrchou@gmail.com>
2018-05-26 15:38:31 -0700

    Fixed file writing logic error in retweet feature

--- run.py
+++ run.py
@@ -32,39 +32,55 @@ async def on_message(message):
     #     msg = '{0.author.mention}\'s birthday recorded.'.format(message)
     #     await client.send_message(message.channel, msg)
 
-    # checks for retweet emojis
-    # file = open('retweeted_messages.txt', 'r+')
-    # retweeted_messages = []
-    # for m in file.readlines():
-    #     retweeted_messages.append(m.strip())
-    # new_messages = []
-    # for c in client.get_all_channels():
-    #     async for m in client.logs_from(client.get_channel(c.id), limit = 100000):
-    #         for r in m.reactions:
-    #             if r.custom_emoji:
-    #                 if r.emoji.name == 'retweet' and r.count > 2 and m.id not in retweeted_messages and message.author != client.user:
-    #                     new_messages.append(m.id)
-    #                     msg = '<:retweet:449394937541427230> x ' + str(r.count)
-    #                     if m.author.nick is None:
-    #                         nickname = m.author.name
-    #                     else:
-    #                         nickname = m.author.nick
-    #                     em = discord.Embed(description=m.content, title="#" + c.name, colour=0000000)
-    #                     em.set_author(name = nickname, icon_url = m.author.avatar_url)
-    #                     await client.send_message(client.get_channel('448621029930303488'),
-    #                                               msg.format(message),
-    #                                               embed = em)
-    # for m in new_messages:
-    #     file.write(m + '\n')
-    # file.close()
+    #checks for retweet emojis
+    file = open('retweeted_messages.txt', 'r+')
+    retweeted_messages = []
+    for m in file.readlines():
+        retweeted_messages.append(m.strip())
+    new_messages = []
+    for c in client.get_all_channels():
+        async for m in client.logs_from(client.get_channel(c.id), limit = 100000):
+            for r in m.reactions:
+                if r.custom_emoji:
+                    if r.emoji.name == 'retweet' and r.count > 2 and m.id not in retweeted_messages and message.author != client.user:
+                        new_messages.append(m.id)
+                        msg = '<:retweet:449394937541427230> x ' + str(r.count)
+                        if m.author.nick is None:
+                            nickname = m.author.name
+                        else:
+                            nickname = m.author.nick
+                        em = discord.Embed(description=m.content, title="#" + c.name, colour=0000000)
+                        em.set_author(name = nickname, icon_url = m.author.avatar_url)
+                        await client.send_message(client.get_channel('448621029930303488'),
+                                                  msg.format(message),
+                                                  embed = em)
+    for m in new_messages:
+        file.write(m + '\n')
+    file.close()
 
 @client.event
 async def on_ready():
+    #write all previous retweeted messages to txt file so they don't appear
+    file = open('retweeted_messages.txt', 'r+')
+    retweeted_messages = []
+    for m in file.readlines():
+        retweeted_messages.append(m.strip())
+    new_messages = []
+    for c in client.get_all_channels():
+        async for m in client.logs_from(client.get_channel(c.id), limit = 100000):
+            for r in m.reactions:
+                if r.custom_emoji:
+                    if r.emoji.name == 'retweet' and r.count > 2 and m.id not in retweeted_messages:
+                        new_messages.append(m.id)
+    for m in new_messages:
+        file.write(m + '\n')
+    file.close()
+
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    for x in client.get_all_channels():
-        print(x)
-        print(x.id)
+    # for x in client.get_all_channels():
+    #     print(x)
+    #     print(x.id)
     print('------')
 client.run(TOKEN)

78fb0e8b2941f0053e5ff9f558e88615f7af1fe7
Audrey Chou <audrchou@gmail.com>
2018-05-27 12:57:27 -0700

    Added image embeds to retweet feature; added purging feature for mods only

--- run.py
+++ run.py
@@ -11,6 +11,23 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # purging
+    if message.content.startswith('!purge '):
+        isMod = False
+        for r in message.author.roles:
+            if r.name == 'Fisting Fairy' or r.name == 'Here is my Glamour Luke':
+                isMod = True
+        if isMod:
+            try:
+                number = int(message.content[7:])
+                await client.purge_from(message.channel, limit=number)
+            except ValueError:
+                msg = 'Enter a real number, you fucking idiot'.format(message)
+                await client.send_message(message.channel, msg)
+        else:
+            msg = 'Nice try, baby bitch'.format(message)
+            await client.send_message(message.channel, msg)
+
     # auto-replies
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
@@ -45,12 +62,16 @@ async def on_message(message):
                     if r.emoji.name == 'retweet' and r.count > 2 and m.id not in retweeted_messages and message.author != client.user:
                         new_messages.append(m.id)
                         msg = '<:retweet:449394937541427230> x ' + str(r.count)
+                        em = discord.Embed(description=m.content, title="#" + c.name, colour=0000000)
                         if m.author.nick is None:
                             nickname = m.author.name
+                            em.set_author(name=nickname, icon_url=m.author.avatar_url)
                         else:
                             nickname = m.author.nick
-                        em = discord.Embed(description=m.content, title="#" + c.name, colour=0000000)
-                        em.set_author(name = nickname, icon_url = m.author.avatar_url)
+                            em.set_author(name=nickname, icon_url=m.author.avatar_url)
+                        if len(m.attachments) > 0:
+                            image_url = m.attachments[0].get('url')
+                            em.set_image(url=image_url)
                         await client.send_message(client.get_channel('448621029930303488'),
                                                   msg.format(message),
                                                   embed = em)
@@ -60,7 +81,7 @@ async def on_message(message):
 
 @client.event
 async def on_ready():
-    #write all previous retweeted messages to txt file so they don't appear
+    # write all previous retweeted messages to txt file so they don't appear
     file = open('retweeted_messages.txt', 'r+')
     retweeted_messages = []
     for m in file.readlines():

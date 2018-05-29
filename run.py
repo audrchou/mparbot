0176f689ba03df4ddb341cb78e673e2156caf945
Audrey Chou <audrchou@gmail.com>
2018-05-28 17:09:11 -0700

    Updated priorities

--- run.py
+++ run.py
@@ -11,6 +11,38 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    #checks for retweet emojis
+    file = open('retweeted_messages.txt', 'r+')
+    retweeted_messages = []
+    for m in file.readlines():
+        retweeted_messages.append(m.strip())
+    new_messages = []
+    for c in client.get_all_channels():
+        async for m in client.logs_from(client.get_channel(c.id), limit = 100000):
+            for r in m.reactions:
+                if r.custom_emoji:
+                    if r.emoji.name == 'retweet' and r.count > 2 and m.id not in retweeted_messages \
+                            and message.author != client.user:
+                        new_messages.append(m.id)
+                        msg = '<:retweet:449394937541427230> x ' + str(r.count)
+                        em = discord.Embed(description=m.content, title="#" + c.name, colour=0000000)
+                        if m.author.nick is None:
+                            nickname = m.author.name
+                            em.set_author(name=nickname, icon_url=m.author.avatar_url)
+                        else:
+                            nickname = m.author.nick
+                            em.set_author(name=nickname, icon_url=m.author.avatar_url)
+                        if len(m.attachments) > 0:
+                            image_url = m.attachments[0].get('url')
+                            em.set_image(url=image_url)
+
+                        await client.send_message(client.get_channel('448621029930303488'),
+                                                  msg.format(message),
+                                                  embed = em)
+    for m in new_messages:
+        file.write(m + '\n')
+    file.close()
+
     # purging
     if message.content.startswith('!purge '):
         isMod = False
@@ -48,37 +80,6 @@ async def on_message(message):
     # if message.content.startswith('!birthday'): #!birthday YYYY-mm-dd
     #     msg = '{0.author.mention}\'s birthday recorded.'.format(message)
     #     await client.send_message(message.channel, msg)
-
-    #checks for retweet emojis
-    file = open('retweeted_messages.txt', 'r+')
-    retweeted_messages = []
-    for m in file.readlines():
-        retweeted_messages.append(m.strip())
-    new_messages = []
-    for c in client.get_all_channels():
-        async for m in client.logs_from(client.get_channel(c.id), limit = 100000):
-            for r in m.reactions:
-                if r.custom_emoji:
-                    if r.emoji.name == 'retweet' and r.count > 2 and m.id not in retweeted_messages \
-                            and message.author != client.user:
-                        new_messages.append(m.id)
-                        msg = '<:retweet:449394937541427230> x ' + str(r.count)
-                        em = discord.Embed(description=m.content, title="#" + c.name, colour=0000000)
-                        if m.author.nick is None:
-                            nickname = m.author.name
-                            em.set_author(name=nickname, icon_url=m.author.avatar_url)
-                        else:
-                            nickname = m.author.nick
-                            em.set_author(name=nickname, icon_url=m.author.avatar_url)
-                        if len(m.attachments) > 0:
-                            image_url = m.attachments[0].get('url')
-                            em.set_image(url=image_url)
-                        await client.send_message(client.get_channel('448621029930303488'),
-                                                  msg.format(message),
-                                                  embed = em)
-    for m in new_messages:
-        file.write(m + '\n')
-    file.close()
 
 @client.event
 async def on_ready():

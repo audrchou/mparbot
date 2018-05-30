955cf415477177d89044f2b860cba7485ba59196
Audrey Chou <audrchou@gmail.com>
2018-05-29 21:29:12 -0700

    Moved retweet feature to background job and temporarily disabled purge feature

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 # https://github.com/Rapptz/discord.py/blob/async/examples/reply.py
 import discord
+import time
+import asyncio
 
-TOKEN = 'NDQ4NTQzNjgwNDU0ODUyNjI4.DeZPaw.RHAWgz1vmD2B_uq7-4HtJuf--Tw'
+# from discord.ext import commands
 
+token = 'NDQ4NTQzNjgwNDU0ODUyNjI4.DeZPaw.RHAWgz1vmD2B_uq7-4HtJuf--Tw'
 client = discord.Client()
 
 @client.event
@@ -11,54 +14,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    #checks for retweet emojis
-    file = open('retweeted_messages.txt', 'r+')
-    retweeted_messages = []
-    for m in file.readlines():
-        retweeted_messages.append(m.strip())
-    new_messages = []
-    for c in client.get_all_channels():
-        async for m in client.logs_from(client.get_channel(c.id), limit = 100000):
-            for r in m.reactions:
-                if r.custom_emoji:
-                    if r.emoji.name == 'retweet' and r.count > 2 and m.id not in retweeted_messages \
-                            and message.author != client.user:
-                        new_messages.append(m.id)
-                        msg = '<:retweet:449394937541427230> x ' + str(r.count)
-                        em = discord.Embed(description=m.content, title="#" + c.name, colour=0000000)
-                        if m.author.nick is None:
-                            nickname = m.author.name
-                            em.set_author(name=nickname, icon_url=m.author.avatar_url)
-                        else:
-                            nickname = m.author.nick
-                            em.set_author(name=nickname, icon_url=m.author.avatar_url)
-                        if len(m.attachments) > 0:
-                            image_url = m.attachments[0].get('url')
-                            em.set_image(url=image_url)
-
-                        await client.send_message(client.get_channel('448621029930303488'),
-                                                  msg.format(message),
-                                                  embed = em)
-    for m in new_messages:
-        file.write(m + '\n')
-    file.close()
-
     # purging
-    if message.content.startswith('!purge '):
-        isMod = False
-        for r in message.author.roles:
-            if r.name == 'Fisting Fairy' or r.name == 'Here is my Glamour Luke':
-                isMod = True
-        if isMod:
-            try:
-                number = int(message.content[7:])
-                await client.purge_from(message.channel, limit=number)
-            except ValueError:
-                msg = 'Enter a real number, you fucking idiot'.format(message)
-                await client.send_message(message.channel, msg)
-        else:
-            msg = 'Nice try, baby bitch'.format(message)
-            await client.send_message(message.channel, msg)
+    # if message.content.startswith('!purge '):
+    #     isMod = False
+    #     for r in message.author.roles:
+    #         if r.name == 'Fisting Fairy' or r.name == 'Here is my Glamour Luke':
+    #             isMod = True
+    #     if isMod:
+    #         try:
+    #             number = int(message.content[7:])
+    #             await client.purge_from(message.channel, limit=number)
+    #         except ValueError:
+    #             msg = 'Enter a real number, you fucking idiot'.format(message)
+    #             await client.send_message(message.channel, msg)
+    #     else:
+    #         msg = 'Nice try, baby bitch'.format(message)
+    #         await client.send_message(message.channel, msg)
 
     # auto-replies
     if message.content.startswith('!hello'):
@@ -81,9 +52,45 @@ async def on_message(message):
     #     msg = '{0.author.mention}\'s birthday recorded.'.format(message)
     #     await client.send_message(message.channel, msg)
 
+async def check_for_retweets():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        file = open('retweeted_messages.txt', 'r+')
+        retweeted_messages = []
+        for m in file.readlines():
+            retweeted_messages.append(m.strip())
+        new_retweets = []
+        for c in client.get_all_channels():
+            async for m in client.logs_from(client.get_channel(c.id), limit=100000):
+                for r in m.reactions:
+                    if r.custom_emoji:
+                        if r.emoji.name == 'retweet' and r.count > 2 and m.id not in retweeted_messages \
+                                and m.author != client.user:
+                            new_retweets.append(m)
+                            msg = '<:retweet:449394937541427230> x ' + str(r.count)
+                            em = discord.Embed(description=m.content, title="#" + c.name, colour=0000000)
+                            if m.author.nick is None:
+                                nickname = m.author.name
+                                em.set_author(name=nickname, icon_url=m.author.avatar_url)
+                            else:
+                                nickname = m.author.nick
+                                em.set_author(name=nickname, icon_url=m.author.avatar_url)
+                            if len(m.attachments) > 0:
+                                image_url = m.attachments[0].get('url')
+                                em.set_image(url=image_url)
+                            await client.send_message(client.get_channel('448621029930303488'),
+                                                      msg,
+                                                      embed=em)
+                            #
+                            #449644920412831755
+        for m in new_retweets:
+            file.write(m.id + '\n')
+        file.close()
+        await asyncio.sleep(60) # task runs every 60 seconds
+
 @client.event
 async def on_ready():
-    # write all previous retweeted messages to txt file so they don't appear
+    #write all previous retweeted messages to txt file so they don't appear
     file = open('retweeted_messages.txt', 'r+')
     retweeted_messages = []
     for m in file.readlines():
@@ -106,4 +113,6 @@ async def on_ready():
     #     print(x)
     #     print(x.id)
     print('------')
-client.run(TOKEN)
+
+client.loop.create_task(check_for_retweets())
+client.run(token)
